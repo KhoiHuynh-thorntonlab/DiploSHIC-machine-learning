18c8f334b3495768e90425f41f6e7077375e9d96
KhoiHuynh-thorntonlab <38586123+KhoiHuynh-thorntonlab@users.noreply.github.com>
2018-09-05 18:24:10 -0700

    Update countoccurence.sh

--- pipeline/countoccurence.sh
+++ pipeline/countoccurence.sh
@@ -11,6 +11,6 @@ done
 for i in $(seq 0 99);
 do
 	echo "0.1window$i |" >> /home/khoih/0.1total.summary.txt
-	cut -f 5 /home/khoih/backup/0.1window$i.txt | sort | uniq -c > /home/khoih/0.1window$i.summary.txt 
+	cut -f 5 /home/khoih/backup/0.1window$i.txt | sort | uniq -c >> /home/khoih/0.1window$i.summary.txt 
 	cat /home/khoih/0.1window$i.summary.txt >> /home/khoih/0.1total.summary.txt
 done
